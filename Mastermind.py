c2bcf99b2002386ce9d8a85ce7b0f8592a03e1c8
MehIsOcto <74482720+MehIsOcto@users.noreply.github.com>
2020-11-15 16:48:13 +1030

    Add files via upload
    Encountering issue with names, trying again later

--- Mastermind.py
+++ Mastermind.py
@@ -99,9 +99,96 @@ class CodeCreation:
     def get_code_list(self):
         return self.code
 
+
+
+class GuessCode(CodeCreation, PlayerNames):
+    def __init__(self):
+        self.guessed = ""
+        self.code = ""
+        self.p2 = ""
+
+    def guessing(self, code, p2):
+
+        convertcode = []
+        convertcode[:0] = code
+
+        print(convertcode)
+        print('')
+
+        print('Welcome ' + p2 +', you can now start to play by guessing the code.')
+        print('Enter a guess by providing four characters and press Enter.')
+        print('')
+
+        self.guessed = 0
+        tries = 0
+
+        while self.guessed == 0:
+
+            tries = tries + 1
+            print('Attempt #' + str(tries))
+            guess = input('')
+
+            if guess == code:
+                print('Congratulations! You broke the code in ' + str(tries) + ' attempt/s.')
+                self.guessed = 1
+
+            elif guess != code:
+                convertcode = []
+                c_index = 0
+                g_list = []
+                g_index = 0
+                feedback = ""
+
+                for entry in code:
+                    convertcode += entry
+                for entry in guess:
+                    g_list += entry
+                
+
+
+                while g_index < 4:
+                    if g_list[g_index] == convertcode[c_index] and g_index == c_index:
+                        feedback += 'B '
+                        g_list[g_index] = ''
+                        g_index += 1
+                        convertcode[c_index] = ''
+                        c_index = 0
+                    elif g_list[g_index] == convertcode[c_index] and g_index != c_index:
+                        if g_list[g_index] == convertcode[g_index]:
+                            feedback += 'B '
+                            g_list[g_index] = ''
+                            g_index += 1
+                            convertcode[c_index] = ''
+                            c_index = 0
+                        elif g_list[c_index] != convertcode[c_index]:
+                            feedback += 'W '
+                            g_list[g_index] = ''
+                            g_index += 1
+                            convertcode[c_index] = '*'
+                            c_index = 0
+                    else:
+                        c_index += 1
+                    if c_index >= 4:
+                        g_index += 1
+                        c_index = 0
+                        
+            print('Feedback on Attempt #' + str(tries) + ': ' + feedback)
+            print('')
+                
+            if tries > 9:
+                print ('You ran out of tries!')
+                self.guessed = 1
+
+
+
+                
 """Class Initalisations"""
 modeSelect = Game_Mode_Selector()
 playSelect = PlayOrQuit()
+player1Select = PlayerNames()
+player2Select = PlayerNames()
+codeSelect = CodeCreation()
+guessedSelect = GuessCode()
 
 
 """Game Loop"""
@@ -116,6 +203,8 @@ player1Select.player1()
 player2Select.player2()
 print('P1 =',player1Select.get_player1_value())
 print('P2 =',player2Select.get_player2_value())
+
+MastermindIntro.intro(PlayerNames, player1Select.get_player1_value(),player2Select.get_player2_value())
 
 codeSelect.createcode()
 print('Code =',codeSelect.get_code_list())
