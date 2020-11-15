981ee335642cb67431551e0d024690f7303eac45
MehIsOcto <74482720+MehIsOcto@users.noreply.github.com>
2020-11-15 16:30:08 +1030

    Add files via upload
    Git had issues, recreating...Added Menu

---
+++ Mastermind.py
@@ -0,0 +1,12 @@
+class Menu:
+    print('')
+
+    print('Welcome to Mastermind!')
+    print('Developed by Ethan Weight')
+    print('COMP 1046 Object-Oriented Programming')
+
+    print('')
+
+
+Menu()
+
